c1727263004eb4823b090e86d3ee03dba1c34960
Benjamin Baker <bjmn.bkr@gmail.com>
2019-11-15 12:06:04 +0000

    commiting for upload

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,35 @@
+class DosesController < ApplicationController
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredient = Ingredient.find(params[:dose][:ingredient_id])
+    @dose.cocktail = @cocktail
+    @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Dropping the seeds"
+Ingredient.destroy_all
+
+puts "Creating the seeds"
+ingredients = []
+
+30.times do
+  ingredients << Faker::Food.ingredient
+  ingredients.uniq!
+  ingredients.each do |i|
+    Ingredient.create(name: i)
+  end
+end
+
+puts "Seeds created"
+# p Ingredient.all
